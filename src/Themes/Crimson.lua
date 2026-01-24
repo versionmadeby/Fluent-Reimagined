0a537ffc6dd8b6a60d1a88bc97efe36a0cf7308f
Woow <nbonney261@gmail.com>
2026-01-24 14:09:59 +0000

    Create Crimson.lua

---
+++ src/Themes/Crimson.lua
@@ -0,0 +1,47 @@
+return {
+	Name = "Crimson",
+	Accent = Color3.fromRGB(180, 50, 50),
+
+	AcrylicMain = Color3.fromRGB(20, 15, 15),
+	AcrylicBorder = Color3.fromRGB(120, 60, 60),
+	AcrylicGradient = ColorSequence.new(Color3.fromRGB(120, 40, 40), Color3.fromRGB(50, 20, 20)),
+	AcrylicNoise = 0.92,
+
+	TitleBarLine = Color3.fromRGB(100, 50, 50),
+	Tab = Color3.fromRGB(160, 70, 70),
+
+	Element = Color3.fromRGB(140, 60, 60),
+	ElementBorder = Color3.fromRGB(60, 30, 30),
+	InElementBorder = Color3.fromRGB(100, 50, 50),
+	ElementTransparency = 0.87,
+
+	ToggleSlider = Color3.fromRGB(140, 60, 60),
+	ToggleToggled = Color3.fromRGB(0, 0, 0),
+
+	SliderRail = Color3.fromRGB(140, 60, 60),
+
+	DropdownFrame = Color3.fromRGB(170, 80, 80),
+	DropdownHolder = Color3.fromRGB(60, 30, 30),
+	DropdownBorder = Color3.fromRGB(50, 25, 25),
+	DropdownOption = Color3.fromRGB(140, 60, 60),
+
+	Keybind = Color3.fromRGB(140, 60, 60),
+
+	Input = Color3.fromRGB(140, 60, 60),
+	InputFocused = Color3.fromRGB(30, 10, 10),
+	InputIndicator = Color3.fromRGB(180, 90, 90),
+
+	Dialog = Color3.fromRGB(60, 30, 30),
+	DialogHolder = Color3.fromRGB(45, 20, 20),
+	DialogHolderLine = Color3.fromRGB(40, 15, 15),
+	DialogButton = Color3.fromRGB(60, 30, 30),
+	DialogButtonBorder = Color3.fromRGB(110, 60, 60),
+	DialogBorder = Color3.fromRGB(90, 40, 40),
+	DialogInput = Color3.fromRGB(70, 35, 35),
+	DialogInputLine = Color3.fromRGB(190, 100, 100),
+
+	Text = Color3.fromRGB(240, 240, 240),
+	SubText = Color3.fromRGB(200, 180, 180),
+	Hover = Color3.fromRGB(160, 80, 80),
+	HoverChange = 0.04,
+}
