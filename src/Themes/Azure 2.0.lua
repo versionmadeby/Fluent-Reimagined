e4c780adba3aa0aa746e03bd9b277d90a6e31482
Woow <nbonney261@gmail.com>
2026-01-27 11:57:27 +0000

    Create Azure 2.0.lua

---
+++ src/Themes/Azure 2.0.lua
@@ -0,0 +1,47 @@
+return {
+	Name = "Azure 2.0",
+	Accent = Color3.fromHex("111D5E"),
+	
+	AcrylicMain = Color3.fromRGB(10, 15, 30),
+	AcrylicBorder = Color3.fromRGB(30, 40, 90),
+	AcrylicGradient = ColorSequence.new(Color3.fromRGB(20, 30, 70), Color3.fromRGB(8, 12, 35)),
+	AcrylicNoise = 0.92,
+	
+	TitleBarLine = Color3.fromRGB(30, 40, 90),
+	Tab = Color3.fromRGB(40, 50, 110),
+	
+	Element = Color3.fromRGB(25, 35, 80),
+	ElementBorder = Color3.fromRGB(15, 25, 60),
+	InElementBorder = Color3.fromRGB(35, 45, 100),
+	ElementTransparency = 0.87,
+	
+	ToggleSlider = Color3.fromRGB(25, 35, 80),
+	ToggleToggled = Color3.fromRGB(17, 29, 94), -- Lighter variant of accent
+	
+	SliderRail = Color3.fromRGB(25, 35, 80),
+	
+	DropdownFrame = Color3.fromRGB(45, 55, 115),
+	DropdownHolder = Color3.fromRGB(15, 25, 60),
+	DropdownBorder = Color3.fromRGB(10, 20, 55),
+	DropdownOption = Color3.fromRGB(25, 35, 80),
+	
+	Keybind = Color3.fromRGB(25, 35, 80),
+	
+	Input = Color3.fromRGB(25, 35, 80),
+	InputFocused = Color3.fromRGB(5, 10, 40),
+	InputIndicator = Color3.fromRGB(50, 60, 130),
+	
+	Dialog = Color3.fromRGB(15, 25, 60),
+	DialogHolder = Color3.fromRGB(10, 20, 50),
+	DialogHolderLine = Color3.fromRGB(8, 18, 45),
+	DialogButton = Color3.fromRGB(15, 25, 60),
+	DialogButtonBorder = Color3.fromRGB(40, 50, 110),
+	DialogBorder = Color3.fromRGB(35, 45, 100),
+	DialogInput = Color3.fromRGB(20, 30, 70),
+	DialogInputLine = Color3.fromRGB(60, 70, 150),
+	
+	Text = Color3.fromRGB(240, 240, 245),
+	SubText = Color3.fromRGB(200, 200, 210),
+	Hover = Color3.fromRGB(40, 50, 110),
+	HoverChange = 0.04,
+}
