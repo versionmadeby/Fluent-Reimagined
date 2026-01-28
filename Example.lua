22fb9ea1d420beb1d17a9d76a15fa5110ec1a6dd
Woow <nbonney261@gmail.com>
2026-01-28 19:02:02 +0000

    This was updated to use my load string which has the new themes.

--- Example.lua
+++ Example.lua
@@ -1,6 +1,6 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Fluent = loadstring(game:HttpGet("https://github.com/versionmadeby/Fluent-Reimagined/releases/download/Test/alt.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/versionmadeby/Fluent-Reimagined/refs/heads/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/versionmadeby/Fluent-Reimagined/refs/heads/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "Fluent " .. Fluent.Version,
